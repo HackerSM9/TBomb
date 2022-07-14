d723d4f4f71c7239b29f298cbbf6cbb20d2eab26
Samarth Magdum (SM9) <101000624+Hackersm9@users.noreply.github.com>
2022-07-14 16:51:34 +0530

    Update provider.py

--- utils/provider.py
+++ utils/provider.py
@@ -15,7 +15,7 @@ class APIProvider:
             PROVIDERS = json.load(open('apidata.json', 'r'))
         except Exception:
             PROVIDERS = requests.get(
-                "https://github.com/Hackersm9/TBomb/raw/master/apidata.json"
+                "https://github.com/Hackersm9/TBomb/raw/main/apidata.json"
             ).json()
         self.config = None
         self.cc = cc
